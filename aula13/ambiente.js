c574c7b7feb8ecc8db1501ef52650a02bc7d242a
Daniel Ataydes <daniel_ataydes@msn.com>
2022-09-29 11:31:16 -0300

    aula14

---
+++ aula13/ambiente.js
@@ -0,0 +1,17 @@
+/* faça o teste lógico e  executa 
+var c = 1
+while (c <= 6) {
+    console.log(`Passo ${c}`)
+    c++
+}
+*/
+
+// executa e faça o teste lógico
+var c = 1
+do {
+    console.log(`passo ${c}`)
+    c++
+} while (c <= 6)
+
+
+
